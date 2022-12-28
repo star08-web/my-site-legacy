b3d839f7bb278266e1cae61deeada69342185f6b
star <87822123+star08-web@users.noreply.github.com>
2022-12-28 21:37:17 +0100

    mobile ready (i think)

--- js/main.js
+++ js/main.js
@@ -1,27 +1,9 @@
-// sidebar stuffs
-let menu = document.querySelector(".menu");
-let sidebar = document.querySelector(".sidebar");
-let hometext = document.querySelector(".hometext");
-let userstext = document.querySelector(".userstext");
-let airplanetext = document.querySelector(".airplanetext");
-let lampadinatext = document.querySelector(".lampadinatext");
-let videocameratext = document.querySelector(".videocameratext");
-let sunnytext = document.querySelector(".sunnytext");
-let icon = document.querySelector(".icon");
-let btn = document.querySelector(".btn-icn")
-let text1 = document.querySelector(".text1");
-let text2 = document.querySelector(".text2");
-let text3 = document.querySelector(".text3");
-let text4 = document.querySelector(".text4");
-let text5 = document.querySelector(".text5");
-let text6 = document.querySelector(".text6");
-function OpenMenu(){
-    sidebar.classList.toggle("openclosemenu");
-    btn.classList.toggle("show");
-    text1.classList.toggle("openclosemenu");        
-    text2.classList.toggle("openclosemenu");        
-    text3.classList.toggle("openclosemenu");        a
-    text4.classList.toggle("openclosemenu");        
-    text5.classList.toggle("openclosemenu");        
-    text6.classList.toggle("openclosemenu");        
-}
+let bar = document.querySelector(".mobbar");
+let toggler = document.querySelector(".mobmenu");
+let moblink = document.querySelector(".moblink")
+let downloads = document.querySelector(".downnw")
+toggler.addEventListener("click", function () {
+  bar.classList.toggle("open");
+  moblink.classList.toggle("show")
+  downloads.classList.toggle("show")
+});
